b87a86bec47dc9ac0f7e087f55620c2876c36ecf
SubhankarMukherjee <connecttosubhankar@gmail.com>
2021-11-21 16:17:33 +0530

    project setup

---
+++ src/main/java/guru/springframework/msscssm/domain/repository/PaymentRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscssm.domain.repository;
+
+import guru.springframework.msscssm.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
+
+}
---
+++ src/main/java/guru/springframework/msscssm/domain/repository/config/StateMachineConfig.java
@@ -0,0 +1,59 @@
+package guru.springframework.msscssm.domain.repository.config;
+
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
+
+import java.util.EnumSet;
+
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)   // initial state
+                .states(EnumSet.allOf(PaymentState.class))   // load all the states
+                .end(PaymentState.AUTH)                      // terminal states
+                .end(PaymentState.PRE_AUTH_ERROR)                // terminal states
+                .end(PaymentState.AUTH_ERROR);                       // terminal states
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
+        //New --> New(No state change) Event->PRE_AUTHORIZE
+        //New --> PRE_AUTH Event->PRE_AUTHORIZE_APPROVED
+        //New --> PRE_AUTH ERROR EVENT-->PRE_AUTHORIZE_DECLINED
+
+
+        transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+                .and()
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+                .and()
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED);
+    }
+
+    //listner and log
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<PaymentState, PaymentEvent> config) throws Exception {
+        StateMachineListenerAdapter<PaymentState, PaymentEvent> adapter = new StateMachineListenerAdapter(){
+            @Override
+            public void stateChanged(State from, State to) {
+                log.info(String.format("stateChanged(from: %s, to: %s)", from, to));
+            }
+        };
+
+        config.withConfiguration()
+                .listener(adapter);
+    }
+}
